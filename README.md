97d86e7e3d61d4b5bd3c4b27563fb654708576bf
BernysCS <192170386+BernysCS@users.noreply.github.com>
2025-05-19 15:13:58 -0600

    crear proyecto de centro medico dayenu

---
+++ README.md
@@ -0,0 +1,3 @@
+# centro_medico_dayenu
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/centro_medico_dayenu/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.centro_medico_dayenu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
